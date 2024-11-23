6b1c9bf98aca819677a9cc31c6b42922e9f4f919
MdAfnanNaushad <mdafnannaushad@gmail.com>
2024-11-23 15:25:49 +0530

    scrolling behavior added

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let a =  document.querySelector('.transforms')
-a.addEventListener('mouseover',()=>{
-    a.innerHTML="Video Editor"
+let a = document.querySelector('.transforms')
+a.addEventListener('mouseover', () => {
+    a.innerHTML = "Video Editor"
+})
+a.addEventListener('mouseleave',()=>{
+    a.innerHTML = "Full Stack Web Developer"
+    a.style.color = "aqua";
+})
+let b = document.querySelector('nav ').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    })
 })
